6f00e97b84a9e659f0a19de4f602c845b8356925
nicholas-leonard <nick@nikopia.org>
2014-06-12 02:56:25 -0400

    added 3D unit test for index

--- test/test.lua
+++ test/test.lua
@@ -148,9 +148,10 @@ function test.std()
 end
 
 function test.index()
-  local sz1 = math.floor(torch.uniform(minsize,maxsize))
-  local sz2 = math.floor(torch.uniform(minsize,maxsize))
-  local x = torch.FloatTensor():rand(sz1, sz2)
+   local sz1 = math.floor(torch.uniform(minsize,maxsize))
+   local sz2 = math.floor(torch.uniform(minsize,maxsize))
+   local sz3 = math.floor(torch.uniform(10,20))
+   local x = torch.FloatTensor():rand(sz1, sz2)
 
   local longIndex = torch.LongTensor{math.floor(torch.uniform(1, sz1)), math.floor(torch.uniform(1, sz1))}
   local index = 1
@@ -165,6 +166,10 @@ function test.index()
   longIndex = torch.LongTensor{math.floor(torch.uniform(1, sz1)), math.floor(torch.uniform(1, sz1))}
   compareFloatAndCuda(x, 'index', index, longIndex)
 
+   x = torch.FloatTensor():rand(sz1,sz2,sz3)
+   index = 3
+   longIndex = torch.randperm(sz3):long()
+   compareFloatAndCuda(x, 'index', index, longIndex)
 end
 
 function test.indexCopy()
