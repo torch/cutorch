8f01efef77a66303ef68785d6ea24d7b54fe412a
soumith <soumith@gmail.com>
2016-02-04 16:40:18 -0500

    lua 5.3 fixes

--- torch/utils.h
+++ torch/utils.h
@@ -44,6 +44,16 @@ TORCH_UNUSED static void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup
 }
 #endif
 
+#if LUA_VERSION_NUM >= 503
+/* one can simply enable LUA_COMPAT_5_2 to be backward compatible.
+However, this does not work when we are trying to use system-installed lua,
+hence these redefines
+*/
+#define luaL_optlong(L,n,d)     ((long)luaL_optinteger(L, (n), (d)))
+#define luaL_optint(L,n,d)  ((int)luaL_optinteger(L, (n), (d)))
+#define luaL_checklong(L,n)     ((long)luaL_checkinteger(L, (n)))
+#define luaL_checkint(L,n)      ((int)luaL_checkinteger(L, (n)))
+#endif
 
 TORCH_API THLongStorage* cutorch_checklongargs(lua_State *L, int index);
 TORCH_API int cutorch_islongargs(lua_State *L, int index);
