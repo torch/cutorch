9aef731946f687e08c2ba4438ab985c433e3044f
Boris Fomitchev <bfomitchev@nvidia.com>
2016-11-15 02:46:49 -0800

    Added some memory allocations to test_shutdown to test  acching allocator

--- test/test_shutdown.lua
+++ test/test_shutdown.lua
@@ -1,11 +1,36 @@
 local Threads = require 'threads'
 require 'cutorch'
 
+local function test_cudaEvent()
+   cutorch.reserveStreams(2)
+   cutorch.setStream(1)
+
+   local t1 = torch.CudaTensor(10000000):zero()
+   local t2 = torch.CudaTensor(1):zero()
+
+   local t1View = t1:narrow(1, 10000000, 1)
+   t1:fill(1)
+   print('Memory usage after some allocations [free memory], [total memory]')
+   print(cutorch.getMemoryUsage())
+
+   -- Event is created here
+   local event = cutorch.Event()
+
+   cutorch.setStream(2)
+
+   -- assert below will fail without this
+   event:waitOn()
+   t2:copy(t1View)
+
+   -- revert to default stream
+   cutorch.setStream(0)
+end
+
 print ("cutorch.hasHalf is ", cutorch.hasHalf)
 
 print('Memory usage before intialization of threads [free memory], [total memory]')
 print(cutorch.getMemoryUsage())
-threads = Threads(100, function() require 'cutorch' end)
+threads = Threads(100, function() require 'cutorch'; test_cudaEvent(); end)
 print('Memory usage after intialization of threads [free memory], [total memory]')
 print(cutorch.getMemoryUsage())
 threads:terminate()
